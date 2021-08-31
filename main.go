2ea5bad18c5243946ced5c5b2acc095d94e3a120
krishpranav <krisna.pranav@gmail.com>
2021-08-31 17:30:31 +0530

    main

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	config := config.Config{}
-	flag.StringVar(&config.CIDR, "c", "172.16.0.1/24", "tun interface CIDR")
+	flag.StringVar(&config.CIDR, "c", "172.16.0.1/24", "vpn interface CIDR")
 	flag.StringVar(&config.LocalAddr, "l", "0.0.0.0:3000", "local address")
 	flag.StringVar(&config.ServerAddr, "s", "0.0.0.0:3001", "server address")
 	flag.StringVar(&config.Key, "k", "6w9z$C&F)J@NcRfWjXn3r4u7x!A%D*G-", "encryption key")
