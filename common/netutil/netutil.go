afee21779a4c9306bd224856471d6abab7cd6bd9
krishpranav <krisna.pranav@gmail.com>
2021-08-31 16:58:24 +0530

    common: netutil

--- common/netutil/netutil.go
+++ common/netutil/netutil.go
@@ -10,10 +10,52 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/gorilla/websocket"
-	"github.com/net-byte/vtun/common/config"
+	"github.com/krishpranav/govpn/common/config"
 	"github.com/songgao/water/waterutil"
 )
 
+func GetAddr(b []byte) (srcAddr string, dstAddr string) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+			srcAddr = ""
+			dstAddr = ""
+		}
+	}()
+	if waterutil.IPv4Protocol(b) == waterutil.TCP {
+		srcIp := waterutil.IPv4Source(b)
+		dstIp := waterutil.IPv4Destination(b)
+		srcPort, dstPort := GetPort(b)
+		src := fmt.Sprintf("%s:%s", srcIp.To4().String(), srcPort)
+		dst := fmt.Sprintf("%s:%s", dstIp.To4().String(), dstPort)
+
+		return src, dst
+	} else if waterutil.IPv4Protocol(b) == waterutil.UDP {
+		srcIp := waterutil.IPv4Source(b)
+		dstIp := waterutil.IPv4Destination(b)
+		srcPort, dstPort := GetPort(b)
+		src := fmt.Sprintf("%s:%s", srcIp.To4().String(), srcPort)
+		dst := fmt.Sprintf("%s:%s", dstIp.To4().String(), dstPort)
+		return src, dst
+	} else if waterutil.IPv4Protocol(b) == waterutil.ICMP {
+		srcIp := waterutil.IPv4Source(b)
+		dstIp := waterutil.IPv4Destination(b)
+		return srcIp.To4().String(), dstIp.To4().String()
+	}
+	return "", ""
+}
+
+func GetPort(b []byte) (srcPort string, dstPort string) {
+	packet := gopacket.NewPacket(b, layers.LayerTypeIPv4, gopacket.Default)
+	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+		tcp, _ := tcpLayer.(*layers.TCP)
+		return tcp.SrcPort.String(), tcp.DstPort.String()
+	} else if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+		udp, _ := udpLayer.(*layers.UDP)
+		return udp.SrcPort.String(), udp.DstPort.String()
+	}
+	return "", ""
+}
 func ConnectWS(config config.Config) *websocket.Conn {
 	scheme := "ws"
 	if config.Protocol == "wss" {
